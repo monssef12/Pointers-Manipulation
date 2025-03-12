f5ebe82aeea2fb7d2b9d104f14b4f2577c1747f4
monssef <moncifbakkalli@gmail.com>
2025-03-12 19:45:41 +0000

    added AllocateArr function

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 void SwapTwoVar(int *a, int *b){ // third variable approach
     int temp = *a;
@@ -11,13 +13,32 @@ void PointerTraverseArr(int arr[], int size){ // using the pointers arithmetic t
     int *pointer = arr;
     while (i < size)
     {
-        printf("the item %d is: %d\n", i, *(pointer + i));
+        printf("the item %d is: %d\n", i, *(pointer + i)); 
         i++;
     }
 }
 
+void AllocateArr(int size){ // this function allocates an array of int with a given size dynamicaly and we assign the values all of that using pointers
+    int *ptr = (int *)malloc(sizeof(int)*size);
+    int *pointer = ptr;
+
+    // reading values
+    for (int i = 0; i < size; i++)
+    {
+        /* code */
+        printf("enter the %d number: ", i+1);
+        scanf("%d", pointer);
+        pointer ++;
+    }
+    for (int i = 0; i < size ; i++)
+    {
+        /* code */
+        printf("the value %d is: %d\n", i + 1, ptr[i]);
+    }
+    
+}
+
 int main(){
-    int arr[5] = {1, 4, 3, 0, 3};
-    PointerTraverseArr(arr, 5);
+    AllocateArr(5);
     return 0;
 }
