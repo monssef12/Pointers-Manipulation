221c664ad2cc02d530ed77d327b8a00a17019653
monssef <moncifbakkalli@gmail.com>
2025-03-17 15:53:57 +0000

    int arr average by passing the pointer to the arr

--- main.c
+++ main.c
@@ -73,9 +73,17 @@ char ReverseString(char *str){
     } 
 }
     
+float GetAverage(int *arr, int size){ // take a pointer to an int arr and return its average
+    float sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += *(arr + i); // or we can do arr[i] instead of *(arr + i)
+    }
+    return sum/size;
+}
+
 int main(){
-    char string[] = "";
-    ReverseString(string);
-    printf("the string after reverse is : %s", string);
+    int arr[5] = {1,1,1,1,1};
+    printf("the average is : %f", GetAverage(&arr[0], 5));
     return 0;
 }
